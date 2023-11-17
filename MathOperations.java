c30a1017f2f36f0230ba77324b9aace529726ca4
seerd33 <148865771+seerd33@users.noreply.github.com>
2023-11-17 08:47:49 +0800

    Add files via upload

---
+++ MathOperations.java
@@ -0,0 +1,31 @@
+import static java.lang.Math.*;
+
+public class MathOperations {
+
+    public static int add(int a, int b) {
+        return addExact(a, b);
+    }
+
+    public static int subtract(int a, int b) {
+        return subtractExact(a, b);
+    }
+
+    public static int multiply(int a, int b) {
+        return multiplyExact(a, b);
+    }
+
+    public static int divide(int a, int b) {
+        return floorDiv(a, b);
+    }
+
+    public static void main(String[] args) {
+        // Demonstrate the math operations here
+        int num1 = 20;
+        int num2 = 5;
+
+        System.out.println("Addition: " + add(num1, num2));
+        System.out.println("Subtraction: " + subtract(num1, num2));
+        System.out.println("Multiplication: " + multiply(num1, num2));
+        System.out.println("Division: " + divide(num1, num2));
+    }
+}
